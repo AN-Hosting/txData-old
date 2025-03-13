c899f2ade17523d617f83243513737166f431a66
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-13 21:44:09 +0100

    Fix vehicule push

--- paris_horizon_base/resources/[qb]/qb-smallresources/config.lua
+++ paris_horizon_base/resources/[qb]/qb-smallresources/config.lua
@@ -470,3 +470,19 @@ Config.Telescopes = {
 	{ model = -656927072, coords = vector3(-588.0742, -706.9901, 116.4024) },
 	{ model = -656927072, coords = vector3(-593.5171, -705.6987, 121.2024) },
 }
+
+-- Vehicule push
+Config.target = true -- Use target system for vehicle push (disables TextUI)
+Config.targetSystem = 'qb-target' -- Target System to use. ox_target, qtarget, qb-target
+Config.Usebones = true -- Use bones for vehicle push
+Config.PushKey = 'E' -- Key to push vehicle
+Config.TurnRightKey = 'Q' -- Keys to turn the vehicle while pushing it.
+Config.TurnLeftKey = 'A' -- Keys to turn the vehicle while pushing it.
+Config.TextUI = false -- Use Text UI for vehicle push
+Config.useOTSkills = false -- Use OT Skills for XP gain from pushing vehicles. Found here: https://otstudios.tebex.io
+Config.maxReward = 20 -- Max amount of xp that can be gained from pushing a vehicle per push, make sure this is the same or less than what is set for strength in your OT_skills config.
+Config.healthMin = 2000.0 -- Minimum health of vehicle to be able to push it.
+
+Config.blacklist = { -- blacklist vehicle models from being pushed.
+    [`phantom`] = true
+}
