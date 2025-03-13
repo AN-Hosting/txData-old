f0752ea70f08fbde391ff04dad8dac528ec11849
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-13 16:56:03 +0100

    Init repot file

---
+++ paris_horizon_base/resources/[Map]/CoreMappings1/fxmanifest.lua
@@ -0,0 +1,57 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+-- Information
+
+Author "TS"
+description "CoreMap - N°1"
+
+dependency '/assetpacks'
+
+files {
+
+"interiorproxies.meta",
+"gtxd.meta"
+}
+
+client_scripts {
+    'uj_lifegLoader.lua'
+  }
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+-- GN_BTA_N
+
+data_file "DLC_ITYP_REQUEST" "stream/GN_BTA_N/affiche_gn.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/GN_BTA_N/cagnes.ytyp"
+
+-- MLO_FRW_3_Hannover
+
+data_file 'DLC_ITYP_REQUEST' 'stream/MLO_FRW_3_Hannover/b3d_hano3_exterior.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/MLO_FRW_3_Hannover/bmpm.ytyp'
+
+-- MONTAGNE
+
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_buildings.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_land.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_props.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_road01.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_int_underpass01.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_int_plazabuilding.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_int_247.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/dnxchiliadtown_int_sheriffoffice.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/MONTAGNE/panneau_pghm.ytyp"
+
+-- AMP
+
+data_file 'DLC_ITYP_REQUEST' 'stream/AMP_Aeroport/texte_aeroport.ytyp'
+
+-- GNMAP
+
+data_file 'DLC_ITYP_REQUEST' 'stream/GNMAP/v_int_40.ytyp'
+
+-- TribunalAdmin
+
+data_file 'DLC_ITYP_REQUEST' 'stream/TribunalAdmin/texte_tribunal_ad.ytyp'
