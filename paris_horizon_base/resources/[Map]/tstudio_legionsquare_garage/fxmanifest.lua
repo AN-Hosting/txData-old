0be3fcb662db693370dfec7eabe9906bf7dc5f80
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-13 16:51:42 +0100

    Init repot file

---
+++ paris_horizon_base/resources/[Map]/tstudio_legionsquare_garage/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+lua54 'yes'
+game "gta5"
+
+author 'tstudio - turbosaif'
+description 'Legion Garage by TStudio'
+version '1.0.0'
+
+this_is_a_map "yes"
+
+escrow_ignore {
+  'stream/ytd/*.ytd',
+  'stream/vanilla/*.*'
+}
+dependency '/assetpacks'
