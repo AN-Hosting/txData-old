003f5d66819376e2994c7ba4e16741cc8dc6ffa3
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-14 11:58:56 +0100

    Ajout des rond point a la place des cubes

---
+++ paris_horizon_base/resources/[Map]/PLACE_CUBE/fxmanifest.lua
@@ -0,0 +1,15 @@
+-------------------------------------------------------------------
+fx_version 'cerulean'
+game { 'gta5' }
+-------------------------------------------------------------------
+description 'TreeSpace - PC'
+version '1.1.0'
+this_is_a_map 'yes'
+-------------------------------------------------------------------
+data_file 'DLC_ITYP_REQUEST' 'stream/ml_plcub_ytyp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/asset_ralenti_ytyp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/asset_plots_ytyp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/separation_milieux_route_ytyp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/antichoc.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/asset_abc_ytyp.ytyp'
+-------------------------------------------------------------------
