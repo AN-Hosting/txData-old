8c884566ea80944c91b3a9d87567207250a21a67
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-14 02:23:06 +0100

    Ajout des commande telephone

--- paris_horizon_base/resources/[script]/ph-menuperso/config.lua
+++ paris_horizon_base/resources/[script]/ph-menuperso/config.lua
@@ -1,7 +1,14 @@
 Config = {}
 
 Config.ServiceCommands = {
-    police = 'service_police',
-    ambulance = 'service_ambulance',
-    taxi = 'service_taxi'
+    gendarmerie_nationale = '19',
+    samu_75 = '15',
+    pompiers_de_paris = '18',
+    avocats_de_paris = '115',
+    police_nationale = '17',
+    norauto = '116',
+    midas = '117',
+    concessionnaire = '118',
+    taxi = '119',
+    orpi = '120'
 }
--- paris_horizon_base/resources/[script]/ph-menuperso/fxmanifest.lua
+++ paris_horizon_base/resources/[script]/ph-menuperso/fxmanifest.lua
@@ -6,8 +6,6 @@ description 'QB-Jobmenu'
 version '1.0.0'
 
 shared_scripts {
-    '@qb-core/shared/locale.lua',
-    'locales/en.lua',
     'config.lua',
 }
 
