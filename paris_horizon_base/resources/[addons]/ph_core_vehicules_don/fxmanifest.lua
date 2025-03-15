5a53de4e47bd6c902f9c2dc0529a1db037342e6f
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-15 12:06:08 +0100

    Creation dun core

--- paris_horizon_base/resources/[addons]/ph_core_vehicules_don/fxmanifest.lua
+++ paris_horizon_base/resources/[addons]/ph_core_vehicules_don/fxmanifest.lua
@@ -1,10 +1,5 @@
--- Resource Metadata
 fx_version 'cerulean'
 game 'gta5'
-
-author 'PLOKMJNB#8692'
-description 'A single resource car pack to eliminate the rest'
-repository 'https://github.com/PLOKMJNB/FiveM-Civ-Car-Pack'
 
 files {
     'data/**/*.meta',
@@ -24,16 +19,11 @@ data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
 data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
 data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*unlocks.meta'
 data_file 'PTFXASSETINFO_FILE' 'data/**/ptfxassetinfo.meta'
-data_file 'AUDIO_GAMEDATA' 'audioconfig/tacumminsb_game.dat'
-data_file 'AUDIO_SOUNDDATA' 'audioconfig/tacumminsb_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tacumminsb'
-data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
-data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
-data_file 'AUDIO_GAMEDATA' 'audioconfig/aq67merm177_game.dat'
-data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq67merm177_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq67merm177'
 
-client_scripts {
-    'vehicle_names.lua',
-}
+data_file 'AUDIO_GAMEDATA' 'audioconfig/v6audiea839_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/v6audiea839_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_v6audiea839'
+
+-- client_scripts {
+--     'vehicle_names.lua',
+-- }
