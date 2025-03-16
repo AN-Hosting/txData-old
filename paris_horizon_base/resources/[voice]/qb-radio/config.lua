5162ec5090097eb9d7d14c1b62ce59f6c0a30b4d
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-16 03:31:42 +0100

    Configuration de la radio

--- paris_horizon_base/resources/[voice]/qb-radio/config.lua
+++ paris_horizon_base/resources/[voice]/qb-radio/config.lua
@@ -4,43 +4,43 @@ Config.RadioItem = 'radio'
 
 Config.RestrictedChannels = {
     [1] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [2] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [3] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [4] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [5] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [6] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [7] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [8] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [9] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     },
     [10] = {
-        police = true,
+        police_nationale = true,
         ambulance = true
     }
 }
