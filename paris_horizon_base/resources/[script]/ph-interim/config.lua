5bba87363bc1077121d0aafd3e836ce2806e9efc
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-18 01:51:20 +0100

    Creation d'un systeme d'intérim

---
+++ paris_horizon_base/resources/[script]/ph-interim/config.lua
@@ -0,0 +1,41 @@
+Config = {}
+
+Config.PedLocation = {
+    position = vector4(-259.41, -964.21, 31.22, 165.21),
+    model = `a_m_m_business_01`
+}
+
+Config.Blip = {
+    sprite = 407, -- Icône du blip
+    color = 47,   -- Couleur du blip
+    scale = 0.7,  -- Taille du blip
+    name = "Agence d'Intérim" -- Nom qui apparaît sur la carte
+}
+
+Config.JobLocations = {
+    {
+        name = "Bûcheron",
+        coords = vector3(-702.83, 5337.6, 69.14), -- À ajuster selon vos coordonnées réelles
+        icon = "🌳"
+    },
+    {
+        name = "Ramassage de Pomme",
+        coords = vector3(-817.48, 859.14, 202.99), -- À ajuster selon vos coordonnées réelles
+        icon = "🍎"
+    },
+    {
+        name = "Ramassage de Framboise",
+        coords = vector3(1302.99, -1343.8, 46.12), -- À ajuster selon vos coordonnées réelles
+        icon = "🌹"
+    },
+    {
+        name = "Livreur Chronopost",
+        coords = vector3(66.27, 122.02, 79.13), -- À ajuster selon vos coordonnées réelles
+        icon = "📦"
+    },
+    {
+        name = "Carrière",
+        coords = vector3(2955.0, 2783.94, 41.1), -- À ajuster selon vos coordonnées réelles
+        icon = "⛏️"
+    }
+}
