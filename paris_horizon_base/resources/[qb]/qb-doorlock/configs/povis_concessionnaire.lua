2f6ad90e7e29a2059e9aef5520c3ca63f8376e30
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-18 02:52:09 +0100

    .

---
+++ paris_horizon_base/resources/[qb]/qb-doorlock/configs/povis_concessionnaire.lua
@@ -0,0 +1,71 @@
+
+
+-- povis_concessionnaire1 créer par Nico
+Config.DoorList['povis_concessionnaire-povis_concessionnaire1'] = {
+    doorRate = 1.0,
+    authorizedJobs = { ['concessionnaire'] = 0 },
+    doors = {
+        {objName = 542428389, objYaw = 0.0, objCoords = vec3(-190.921128, -1158.997192, 23.934190)},
+        {objName = 542428389, objYaw = 180.00001525878, objCoords = vec3(-188.324386, -1158.997192, 23.934190)}
+    },
+    locked = true,
+    doorLabel = 'povis_concessionnaire',
+    doorType = 'double',
+    distance = 2,
+}
+
+-- povis_concessionnaire2 créer par Nico
+Config.DoorList['povis_concessionnaire-povis_concessionnaire2'] = {
+    doorRate = 1.0,
+    authorizedJobs = { ['concessionnaire'] = 0 },
+    doors = {
+        {objName = 1901183774, objYaw = 89.999977111816, objCoords = vec3(-198.988830, -1160.771240, 23.907038)},
+        {objName = 1901183774, objYaw = 270.00003051758, objCoords = vec3(-198.977386, -1163.355346, 23.907038)}
+    },
+    locked = true,
+    doorLabel = 'povis_concessionnaire',
+    doorType = 'double',
+    distance = 2,
+}
+
+-- povis_concessionnaire3 créer par Nico
+Config.DoorList['povis_concessionnaire-povis_concessionnaire3'] = {
+    doorRate = 1.0,
+    authorizedJobs = { ['concessionnaire'] = 0 },
+    locked = true,
+    doorLabel = 'povis_concessionnaire',
+    objYaw = 0.0,
+    distance = 2,
+    objCoords = vec3(-192.069946, -1178.585816, 23.233090),
+    fixText = false,
+    doorType = 'door',
+    objName = -35610440,
+}
+
+-- povis_concessionnaire4 créer par Nico
+Config.DoorList['povis_concessionnaire-povis_concessionnaire4'] = {
+    doorRate = 1.0,
+    authorizedJobs = { ['concessionnaire'] = 0 },
+    locked = true,
+    doorLabel = 'povis_concessionnaire',
+    objYaw = 0.0,
+    distance = 4,
+    objCoords = vec3(-176.960006, -1177.718262, 23.953900),
+    fixText = false,
+    doorType = 'garage',
+    objName = -550347177,
+}
+
+-- povis_concessionnaire5 créer par Nico
+Config.DoorList['povis_concessionnaire-povis_concessionnaire5'] = {
+    doorRate = 1.0,
+    authorizedJobs = { ['concessionnaire'] = 0 },
+    locked = true,
+    doorLabel = 'povis_concessionnaire',
+    objYaw = 270.00003051758,
+    distance = 2,
+    objCoords = vec3(-198.977386, -1175.819580, 23.907038),
+    fixText = false,
+    doorType = 'door',
+    objName = 1901183774,
+}
