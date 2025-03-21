19dbbf2078ea420fa6b5fcd39344134721bf2b20
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-21 14:14:36 +0100

    .

---
+++ paris_horizon_base/resources/[qb]/qb-doorlock/configs/dzmafia.lua
@@ -0,0 +1,127 @@
+
+
+-- dzmafia créer par Minox
+Config.DoorList['dzmafia-dzmafia'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4825.145996, -4299.538086, 4.850028),
+    objName = 742146965,
+    doorLabel = 'dzmafia',
+    objYaw = 335.0,
+}
+
+-- dzmafia2 créer par Minox
+Config.DoorList['dzmafia-dzmafia2'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4840.975586, -4283.777832, 7.492028),
+    objName = 2009233619,
+    doorLabel = 'dzmafia',
+    objYaw = 245.00001525878,
+}
+
+-- dzmafia3 créer par Minox
+Config.DoorList['dzmafia-dzmafia3'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4838.271484, -4289.575196, 7.492028),
+    objName = 2009233619,
+    doorLabel = 'dzmafia',
+    objYaw = 245.00001525878,
+}
+
+-- dzmafia4 créer par Minox
+Config.DoorList['dzmafia-dzmafia4'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4833.362792, -4273.017090, 7.440026),
+    objName = -1982806283,
+    doorLabel = 'dzmafia',
+    objYaw = 155.00001525878,
+}
+
+-- dzmafia5 créer par Minox
+Config.DoorList['dzmafia-dzmafia5'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4822.132812, -4274.987792, 7.492028),
+    objName = 2009233619,
+    doorLabel = 'dzmafia',
+    objYaw = 64.999992370606,
+}
+
+-- dzmafia6 créer par Minox
+Config.DoorList['dzmafia-dzmafia6'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4819.428222, -4280.788086, 7.492028),
+    objName = 2009233619,
+    doorLabel = 'dzmafia',
+    objYaw = 64.999992370606,
+}
+
+-- dzmafia7 créer par Minox
+Config.DoorList['dzmafia-dzmafia7'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4851.547852, -4261.290040, 5.976028),
+    objName = -2112609943,
+    doorLabel = 'dzmafia',
+    objYaw = 335.0,
+}
+
+-- dzmafia8 créer par Minox
+Config.DoorList['dzmafia-dzmafia8'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4856.022950, -4268.895996, 5.470024),
+    objName = -1751877127,
+    doorLabel = 'dzmafia',
+    objYaw = 64.999992370606,
+}
+
+-- dzmafia9 créer par Minox
+Config.DoorList['dzmafia-dzmafia9'] = {
+    authorizedGangs = { ['dzmafia'] = 0 },
+    fixText = false,
+    doorType = 'sliding',
+    distance = 5,
+    locked = true,
+    doorRate = 1.0,
+    objCoords = vec3(4847.786622, -4280.257812, 7.180030),
+    objName = 392414576,
+    doorLabel = 'dzmafia',
+    objYaw = 335.0,
+}
