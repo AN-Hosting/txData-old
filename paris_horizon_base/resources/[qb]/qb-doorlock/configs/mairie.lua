8e2cd2ce09cd3939088a84c05565ee4322fd4b64
Nico-du-34 <renardinio1237034@gmail.com>
2025-03-23 11:27:52 +0100

    ..

---
+++ paris_horizon_base/resources/[qb]/qb-doorlock/configs/mairie.lua
@@ -0,0 +1,15 @@
+
+
+-- mairie1 créer par Minox
+Config.DoorList['mairie-mairie1'] = {
+    locked = true,
+    doors = {
+        {objName = 73386408, objYaw = 179.99998474122, objCoords = vec3(-554.255188, -618.038146, 35.049850)},
+        {objName = 73386408, objYaw = 0.0, objCoords = vec3(-556.854004, -618.038146, 35.049850)}
+    },
+    doorType = 'double',
+    authorizedJobs = { ['Maire'] = 0 },
+    doorLabel = 'mairie',
+    doorRate = 1.0,
+    distance = 3,
+}
