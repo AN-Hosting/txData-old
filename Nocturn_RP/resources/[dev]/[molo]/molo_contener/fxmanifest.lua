2f7535db91278516b1a759a33d0f8c9872c38975
Nico-du-34 <renardinio1237034@gmail.com>
2025-04-05 21:50:23 +0200

    N - Ajout molo

---
+++ Nocturn_RP/resources/[dev]/[molo]/molo_contener/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map "yes"
+lua54 'yes'
+
+client_scripts {
+    'npc.lua'
+}
+
+escrow_ignore {
+  'npc.lua'
+
+  }
+
+dependency '/assetpacks'
