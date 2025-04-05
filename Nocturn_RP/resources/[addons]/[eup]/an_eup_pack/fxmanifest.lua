a073d0281425bee8a1713e658b3a1fb79ec35ff3
Nico-du-34 <renardinio1237034@gmail.com>
2025-04-05 21:32:09 +0200

    N - Ajout des eup

---
+++ Nocturn_RP/resources/[addons]/[eup]/an_eup_pack/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Elevio Modifications
+
+fx_version 'bodacious'
+game { 'gta5' }
+
+author 'Elevio Modifications'
+description 'Blaine County Sheriffs EUP'
+version '1.0.0'
+
+files {
+  'mp_m_freemode_01_mp_m_embcsoeup.meta',
+  'mp_f_freemode_01_mp_f_embcsoeup.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_embcsoeup.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_embcsoeup.meta'
