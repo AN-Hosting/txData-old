5ef8bd648a5b6fdfecfea72ddb28b56bfebba7bb
Nico-du-34 <renardinio1237034@gmail.com>
2025-04-05 21:47:43 +0200

    N - Ajout molo

---
+++ Nocturn_RP/resources/[dev]/[molo]/molo_davispd/npc.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+    while (true) do
+		ClearAreaOfPeds(234.93, -1383.41, 29.45, 100.0, 1)
+        Citizen.Wait(500)
+    end
+end)
+
---
+++ Nocturn_RP/resources/[dev]/[molo]/molo_mountchilliad/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+games { 'gta5' }
+lua54 'yes'
+this_is_a_map "yes"
+
+client_script("cl_bounds.lua")
+
+client_script {
+    'client/*.lua',
+    'config.lua'
+}
+
+dependency '/assetpacks'
+escrow_ignore {
+  'config.lua'
+
+  }
+dependency '/assetpacks'
