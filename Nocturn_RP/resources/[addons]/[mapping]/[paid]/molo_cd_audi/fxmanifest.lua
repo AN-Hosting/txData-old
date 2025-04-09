a23366ea090759535482a2e589b3aef718d751c2
Nico-du-34 <renardinio1237034@gmail.com>
2025-04-10 01:52:38 +0200

    N - Mapping

---
+++ Nocturn_RP/resources/[addons]/[mapping]/[paid]/molo_cd_audi/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map "yes"
+
+
+data_file 'AUDIO_GAMEDATA' 'audio/molo_cd_door_game.dat'
+
+
+files {
+  'audio/molo_cd_door_game.dat151.rel',
+
+}
+
+escrow_ignore {
+
+  
+  }
+
+dependency '/assetpacks'
