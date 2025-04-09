96f39ed5ea0a26f8e6f4d7064b0f46d1512694ea
Nico-du-34 <renardinio1237034@gmail.com>
2025-04-10 01:54:38 +0200

    N - Vehicules

---
+++ Nocturn_RP/resources/[addons]/[vehicules]/core_boby/fxmanifest.lua
@@ -0,0 +1,66 @@
+fx_version 'cerulean'
+
+games {'gta5'}
+
+
+client_script {
+	'car_names.lua'
+}
+
+files {
+    'data/**/*.meta',
+	--'audioconfig/*.dat151.rel',
+  	--'audioconfig/*.dat54.rel',
+	--'sfx/**/*.awc'
+}
+
+data_file 'HANDLING_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/*.meta'
+data_file 'CARCOLS_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'data/**/*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*.meta'
+
+-------- CAR SOUND --------
+
+--[[ data_file 'AUDIO_GAMEDATA' 'audioconfig/aventadorv12_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bcat_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/cw2019_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/dilettantedx_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/dlcsavanna_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f10m5_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lspdinsurgent_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s85b50_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sahpinsurgent_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sentinelsg4_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/stratumc_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sultanrsv8_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tamustanggt50p_game.dat'
+
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aventadorv12_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/cw2019_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/dilettantedx_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f10m5_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s85b50_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sahpinsurgent_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sentinelsg4_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/stratumc_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sultanrsv8_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tamustanggt50p_sounds.dat'
+
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aventadorv12'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cw2019'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_dilettantedx'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f10m5'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s85b50'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sentinelsg4'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tamustanggt50p'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_v8sultanrs'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_zircoflow' ]]
