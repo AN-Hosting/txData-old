d6c7909c5de7713118c8259014d0c2be87a5d4de
Nico-du-34 <renardinio1237034@gmail.com>
2025-05-13 00:42:48 +0200

    .

---
+++ Nocturn_RP/resources/[dev]/legionsquare/__resource.lua
@@ -0,0 +1,78 @@
+--[[
+
+-------------------------------------------------------------------------------------------
+
+  ___    ___    _  _   ___      _      ___     _     _  __  ___ 
+ | _ )  / _ \  | \| | |   \    | |    | __|   /_\   | |/ / / __|
+ | _ \ | (_) | | .` | | |) |   | |__  | _|   / _ \  | ' <  \__ \
+ |___/  \___/  |_|\_| |___/    |____| |___| /_/ \_\ |_|\_\ |___/
+                                                                 
+
+-------------------------------------------------------------------------------------------
+Password: BondLeaks
+
+JOIN HERE IF YOU WANT MORE EXCLUSIVE FIVEM LEAKS
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+
+OR OUR SITE
+
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+
+
+]]--
+
+
+resource_manifest_version "44febabe-d386-4dl8-afbe-5e627f4af937"
+
+this_is_a_map 'yes'--[[
+
+-------------------------------------------------------------------------------------------
+
+  ___    ___    _  _   ___      _      ___     _     _  __  ___ 
+ | _ )  / _ \  | \| | |   \    | |    | __|   /_\   | |/ / / __|
+ | _ \ | (_) | | .` | | |) |   | |__  | _|   / _ \  | ' <  \__ \
+ |___/  \___/  |_|\_| |___/    |____| |___| /_/ \_\ |_|\_\ |___/
+                                                                 
+
+-------------------------------------------------------------------------------------------
+Password: BondLeaks
+
+JOIN HERE IF YOU WANT MORE EXCLUSIVE FIVEM LEAKS
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+https://discord.gg/CeF44BBPkK
+
+OR OUR SITE
+
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+https://bond5m.com
+
+
+]]--
+
+
