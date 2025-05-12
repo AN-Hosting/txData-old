bacba236ba5aa52411bc7a9b50b3bb35ddc8561b
Nico-du-34 <renardinio1237034@gmail.com>
2025-05-13 00:41:56 +0200

    .

---
+++ Nocturn_RP/resources/[addons]/[vehicules]/flatbed3/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'data/carvariations.meta',
+	'data/dlctext.meta',
+	'data/handling.meta',
+	'data/vehicles.meta',
+	'data/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'data/dlctext.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
