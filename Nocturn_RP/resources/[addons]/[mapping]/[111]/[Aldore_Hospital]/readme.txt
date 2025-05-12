b2683f91c122cdaa1301ad9cb889246683a0f89f
Nico-du-34 <renardinio1237034@gmail.com>
2025-05-13 00:40:55 +0200

    .

--- Nocturn_RP/resources/[addons]/[mapping]/[111]/[Aldore_Hospital]/readme.txt
+++
@@ -1,2 +0,0 @@
-Preview : https://youtu.be/CILfAm5sUG4?si=doRuOODYWHG828GW
-By : https://discord.gg/pqM43gZfMN 
---
+++ Nocturn_RP/resources/[addons]/[mapping]/moreo_cafecutev2/fxmanifest.lua
@@ -0,0 +1,8 @@
+
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'MoreoDesign'
+description 'CafeCute V2'
+version '2.0.0'
+this_is_a_map 'yes'
