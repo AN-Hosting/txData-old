0cbfbefbc6c8a372277aee1dd67d477901adda6f
Nico-du-34 <renardinio1237034@gmail.com>
2025-05-13 00:40:16 +0200

    .

---
+++ Nocturn_RP/resources/[addons]/[mapping]/[paid]/MZ3D JDM Bennys Local 2.pack/fxmanifest.lua
@@ -0,0 +1,52 @@
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/m3d_prglogo.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_glassjdm2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_glassjdm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/bkr_biker_dlc_int_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_residential.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/hei_dlc_heist_police.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/bkr_biker_dlc_int_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_dlc_int_03_ch.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_lev_des.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ex_int_mp_h_accessories_ex.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_int_41.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_vent.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_ptp1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_pdp2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_logo_interior.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_windjdmmec.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_magaz.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_deskreu.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_tyres.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_edrimss.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_desk1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_prod2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_armar2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_armar1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_boxs1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_quadr.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_quadr.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_recepjd.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_pdmhack.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_palm.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_desk1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_edrimss.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ba_dlc_ba_int2_engine_hoist001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_gas_rack01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz_logo_exterior.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_chairrbnc.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_boxs2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mz3d_bennyslogo.ytyp'
+
+
+
+
+dependency '/assetpacks'
--- Nocturn_RP/resources/[addons]/[mapping]/[paid]/molo_legionsquare/npc.lua
+++
@@ -1,7 +0,0 @@
-Citizen.CreateThread(function()
-    while (true) do
-		ClearAreaOfPeds(260.81, -992.82, 35.57, 500.0, 1)
-        Citizen.Wait(500)
-    end
-end)
-
