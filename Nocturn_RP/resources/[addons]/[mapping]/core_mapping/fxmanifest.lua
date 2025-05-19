8debf39998326d09c617736cfa380d25a70bbdb1
Nico-du-34 <renardinio1237034@gmail.com>
2025-05-19 09:26:51 +0200

    N - Creation dun core mapping

--- Nocturn_RP/resources/[addons]/[mapping]/core_mapping/fxmanifest.lua
+++ Nocturn_RP/resources/[addons]/[mapping]/core_mapping/fxmanifest.lua
@@ -1,3 +1,53 @@
 fx_version 'cerulean'
 game 'gta5'
 this_is_a_map 'yes'       
+replace_level_meta 'gta5'
+
+data_file 'TIMECYCLEMOD_FILE' 'venice_pd/gabz_mrpd_timecycle.xml'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'venice_pd/interiorproxies.meta'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'gabz_popsdiner/interiorproxies.meta'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'casino/interiorproxies.meta'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'bahama/interiorproxies.meta'
+data_file 'TIMECYCLEMOD_FILE' 'ajaxon_mapdata/ajaxon_timecycle.xml'
+data_file "AUDIO_GAMEDATA" "ajaxon_mapdata/game.dat"
+
+
+
+files {
+	'venice_pd/gabz_mrpd_timecycle.xml',
+	'venice_pd/interiorproxies.meta',
+    'marlonstudio_sandypawn/gta5.meta',
+    'marlonstudio_sandypawn/water.xml',
+    'gabz_popsdiner/interiorproxies.meta',
+    "casino/interiorproxies.meta",
+    'bahama/interiorproxies.meta',
+    'ajaxon_mapdata/ajaxon_timecycle.xml',
+    'ajaxon_mapdata/gta5.meta',
+    'ajaxon_mapdata/doortuning.ymt',
+    'ajaxon_mapdata/game.dat151.rel',
+}
+
+client_script {
+    "venice_pd/gabz_mrpd_entitysets.lua",
+    'ajaxon_mapdata/ajaxon_entityset.lua',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_accs_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_animated_walls.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_cabinets.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_cards.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casinochips.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_03.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_04.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_05.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_collectibles.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_disposal.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_door_break.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_machinery.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_screens.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_storage.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_tables.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'casino_props.ytyp'
--- Nocturn_RP/resources/[addons]/[mapping]/marlonstudio_sandypawn/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'bodacious'
-games { 'gta5' }
-
-
-replace_level_meta 'gta5'
-files {
-    'gta5.meta',
-    'water.xml'
-}
