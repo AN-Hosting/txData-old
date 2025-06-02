7d77013ce8b1776e6a6c455487d34bc550d35ba5
Nico-du-34 <renardinio1237034@gmail.com>
2025-06-02 13:01:05 +0200

    M modification nc-petshop

---
+++ Nocturn_RP/resources/[standalone]/nc-petshop/shared.lua
@@ -0,0 +1,77 @@
+Config = {}
+
+-- Pour la Police --
+Config.K9Message = 'Le chien policier a détecté quelque chose !' -- Si le chien policier trouve quelque chose
+-- Fin Police -- 
+
+
+Config.SearchableItems = {
+
+['IllegalItems'] = {
+
+	['weed'] = 10,
+	['coke'] = 1,
+	['bread'] = 1,
+	['water'] = 1,
+},
+}
+
+
+Config.TennisBallPrice = 200
+
+Config.IllnessChance = 5 -- Chance de tomber malade (Pour les animaux) - Si Police, a deux chances de tomber malade
+
+
+Config.HealPrice = 500
+
+Config.FoodPrice = 250
+Config.FoodItem = 'petfood' -- Nom de l'item dans la base de données
+
+Config.TennisBallPrice = 100
+
+Config.Pet1 = 'a_c_cat_01'
+Config.Pet1Price = 500
+Config.Pet1Label = 'Chat'
+
+Config.Pet2 = 'a_c_rabbit_01'
+Config.Pet2Price = 500
+Config.Pet2Label = 'Lapin'
+
+Config.Pet3 = 'a_c_poodle'
+Config.Pet3Price = 500
+Config.Pet3Label = 'Caniche'
+
+Config.Pet4 = 'a_c_pug'
+Config.Pet4Price = 500
+Config.Pet4Label = 'Carlin'
+
+Config.Pet5 = 'a_c_husky'
+Config.Pet5Price = 2000
+Config.Pet5Label = 'Husky'
+
+Config.Pet6 = 'a_c_rottweiler'
+Config.Pet6Price = 2000
+Config.Pet6Label = 'Rottweiler'
+
+Config.Pet7 = 'a_c_hen'
+Config.Pet7Price = 4000
+Config.Pet7Label = 'Poulet Géant'
+
+
+Config.Pet8 = 'a_c_retriever'
+Config.Pet8Price = 2000
+Config.Pet8Label = 'Retriever'
+
+Config.Pet9 = 'a_c_westy'
+Config.Pet9Price = 500
+Config.Pet9Label = 'Terrier des Highlands'
+
+Config.Pet10 = 'a_c_shepherd'
+Config.Pet10Price = 500
+Config.Pet10Label = 'Berger'
+
+Config.Pet11 = 'a_c_pig'
+Config.Pet11Price = 1000
+Config.Pet11Label = 'Cochon'
+
+
